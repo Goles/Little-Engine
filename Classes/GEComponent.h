d5db85003d638a6f0b9f06aa770c9da8c822811f
Nicolas Goles <nicog@GandosMB-PRO.local>
2009-11-25 19:57:28 -0300

    NEW: 	Commited GEComponent and others...
    _NG

---
+++ Classes/GEComponent.h
@@ -0,0 +1,34 @@
+//
+//  GEComponent.h
+//  Particles_2
+//
+//  Created by Nicolas Goles on 11/25/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#include "GameEntity.h"
+#include <string>
+
+class GEComponent
+{
+	//GEComponent Interface
+public:
+	typedef std::string gec_id_type;
+	
+	//Constructor & Destructor
+	GEComponent():ownerGE(0){}
+	virtual ~GEComponent(){}
+	
+	//Action Methods
+	virtual const gec_id_type& componentID() const = 0;
+	virtual const gec_id_type& familyID() const = 0;
+	
+	virtual void update() {}
+	
+	//Getters and setters.
+	void setOwnerGE(GameEntity *gE){ ownerGE = gE; }
+	GameEntity *getOwnerGE() const { return ownerGE; }
+	
+private:
+	GameEntity *ownerGE;
+};
---
+++ Classes/GEComponent.mm
@@ -0,0 +1,9 @@
+//
+//  GEComponent.mm
+//  Particles_2
+//
+//  Created by Nicolas Goles on 11/25/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "GEComponent.h"
