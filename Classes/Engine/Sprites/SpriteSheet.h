bac484dcf95d5f7d3b2b9bd087963b2be5de36e7
Francisco Arevalo <farevalo@gandogames.com>
2010-09-06 20:24:20 -0400

    NEW: 	- Documented SpriteSheet.h _FA

--- Classes/Engine/Sprites/SpriteSheet.h
+++ Classes/Engine/Sprites/SpriteSheet.h
@@ -18,22 +18,71 @@ class SpriteSheet
 public:
 	
 	//Action Methods
+	/** Takes a spriteSheet, defines a sprite size, and calculates horizontal and vertical numbers 
+	    @param spriteSheet Image containing several sprites
+	    @param spriteWidth Width of each sprite
+	    @param spriteHeight Height of each sprite
+	    @param spriteSpacing Separation between sprites
+	    @remarks Details of the implementation of this calculation can be found in SpriteSheet.mm:13
+	*/
 	void	initWithImage(Image *spriteSheet, GLuint spriteWidth, GLuint spriteHeight, GLuint spriteSpacing);
+	/** Wrapper for previous method, takes a filename string, validates it and loads the contents into an Image.
+	*/
 	void	initWithImageNamed(const std::string &inTextureName, GLuint spriteWidth, GLuint spriteHeight, GLuint spacing, float imageScale);
-	Image*	getSpriteAt(GLuint x, GLuint y);
-	void	renderSpriteAt(GLuint x, GLuint y, CGPoint point, BOOL isCenterOfImage);
-	CGPoint	getOffsetForSpriteAt(int x, int y);
-	Quad2*	getTextureCoordsForSpriteAt(GLuint x, GLuint y);
-	Quad2*	getVerticesForSpriteAt(GLuint x, GLuint y, CGPoint point, BOOL isCenterOfImage);
+	/** Calculates a point with coordinates for the selected sprite, and then passes it to getSubImage with the correct sprite size.
+	    @returns The subImage as a new Image object.
+	    @param horizontalNumber selects a sprite in the nth column
+	    @param verticalNumber selects a sprite in the mth row
+	*/
+	Image*	getSpriteAt(GLuint horizontalNumber, GLuint verticalNumber);
+	/** Wrapper for renderSubImage, translates hNum and vNum into coordinates, and passes directly to renderSubImage method.
+	    @param point Render coordinates
+	*/
+	void	renderSpriteAt(GLuint horizontalNumber, GLuint verticalNumber, CGPoint point, BOOL isCenterOfImage);
+	/** Calculates the coordinates for the selected sprite
+	    @returns CGPoint with coordinates
+	*/	
+	CGPoint	getOffsetForSpriteAt(int horizontalNumber, int verticalNumber);
+	/** Wrapper for calculateTexCoordsAtOffset, translates hNum and vNum, and passes them to calculateTexCoordsAtOffset.
+	    @returns Quad2 with texture coordinates.
+	*/
+	Quad2*	getTextureCoordsForSpriteAt(GLuint horizontalNumber, GLuint verticalNumber);
+	
+	/** Wrapper for calculateVertices, NEEDS REVIEW! 
+	    <br>
+	    In SpriteSheet.mm, line 53:
+	        <br>
+	        Quad2*	SpriteSheet::getVerticesForSpriteAt(GLuint horizontalNumber, GLuint verticalNumber, CGPoint point, BOOL isCenterOfImage)<br>
+            {<br>
+	            sheetImage->calculateVertices(point, spriteWidth, spriteHeight, isCenterOfImage);<br>
+	            return sheetImage->getVertex();<br>
+            }<br>
+        
+        From what I see, there is no use for hNum and vNum, since we ask for "point" and pass it straight to calculateVertices.
+        However, this is not consistent with the format used in the last function calls.
+        What I think we should be doing here (and do correct me if I'm wrong), is similar to what we do in
+        getTextureCoordsForSpriteAt; use getOffsetForSpriteAt to map hNum and vNum to a pixel coord, and pass
+        that point to calculateVertices. We seem to be doing nothing of the sort.
+        _FA
+	*/
+	Quad2*	getVerticesForSpriteAt(GLuint horizontalNumber, GLuint verticalNumber, CGPoint point, BOOL isCenterOfImage);
 	
 	//Getters
+	/** Returns sheetImage Image object */
 	Image*	getSheetImage();
+	/** Getter for spriteWidth */
 	GLuint	getSpriteWidth();
+	/** Getter for spriteHeight */
 	GLuint	getSpriteHeight();
+	/** Getter for spacing */
 	GLuint	getSpacing();
+	/** Getter for horizontalNumber */
 	int		getHorizontalNumber();
+	/** Getter for verticalNumber */
 	int		getVerticalNumber();
+	/** Getter for qv */
 	Quad2*	getVertices();
+	/** Getter for tc */
 	Quad2*	getTexCoords();
 	
 private:
